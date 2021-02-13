9f150a78cc39b68881dee3a5188eec8f167382a8
danila-art <57034590+danila-art@users.noreply.github.com>
2021-02-13 12:53:22 +0300

    Add files via upload

--- js/jsScript.js
+++ js/jsScript.js
@@ -19,18 +19,6 @@ function noneModuleWindowArticles() {
     if (getComputedStyle(noneArticles).display == 'none') {
         noneArticles.style.display = 'flex';
         button__open_moduleWindow.firstElementChild.innerHTML = 'Скрыть';
-        let button__open_moduleWindowRect = button__open_moduleWindow.getBoundingClientRect();
-        let bodyRect1 = document.body.getBoundingClientRect();
-        let buttonHeight = button__open_moduleWindowRect.top - bodyRect1.top;
-        let noneArticlesHeight = noneArticles.clientHeight;
-        console.log(bodyRect1);
-        console.log(button__open_moduleWindowRect);
-        console.log(buttonHeight);
-        console.log(noneArticlesHeight);
-            window.scrollTo({
-                top: buttonHeight,
-                behavior: "smooth"
-            })
     } else {
         noneArticles.style.display = 'none';
         button__open_moduleWindow.firstElementChild.innerHTML = 'Показать ещё';
@@ -43,13 +31,6 @@ function noneModuleWindowPortfolio() {
     if (getComputedStyle(noneWindowPortfolio).display == 'none') {
         noneWindowPortfolio.style.display = 'block';
         buttonOpenPortfolioModule.firstElementChild.innerHTML = 'Скрыть';
-        let buttonPortfolioRect = buttonOpenPortfolioModule.getBoundingClientRect();
-        let bodyRect = document.body.getBoundingClientRect();
-        let buttonHeight = buttonPortfolioRect.top - bodyRect.top;
-        window.scrollTo({
-            top: buttonHeight + 600,
-            behavior: "smooth"
-        })
     } else {
         noneWindowPortfolio.style.display = 'none';
         buttonOpenPortfolioModule.firstElementChild.innerHTML = 'Показать ещё';
